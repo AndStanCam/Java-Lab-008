b401d57af65f9c44b31d83d32adb4f071b101bfc
unxok <anderluke707@gmail.com>
2023-03-24 22:25:41 -0700

    Completed assignment

--- src/FileStats.java
+++ src/FileStats.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.io.*;
 
 public class FileStats {
@@ -16,7 +18,20 @@ public class FileStats {
          * throw new FileNotFoundException(String.format("File: %s does not exist.", f.getName()));
          */
 
+        if (!f.exists()) {
+            throw new FileNotFoundException(String.format("File: %s does not exist.", f.getName()));
+        }
+
+
         // Initialize FileStats' instance variables.
+
+        this.f = f;
+        this.skipWhiteSpace = skipWhiteSpace;
+
+        numLines = 0;
+        numChars = 0;
+        numWords = 0;
+
     }
 
     // **You will need to call this method!!!**
@@ -49,12 +64,15 @@ public class FileStats {
     // This method should take a line and count the number of characters in that line.
     private static int countChars(String line, boolean skipWhiteSpace) {
         // 1. If skipWhiteSpace is true, use the removeSpaces method to remove whitespace from the line.
-
+                if (skipWhiteSpace) {
+                   return (removeSpaces(line)).length();
+                }
         // 2. Now write a loop to count the number of characters in the line.
         //    a. HINT: to get the length of a String, use its .length() method!
 
         // 3. Return the count of characters.
         //    a. HINT: If whitespace isn't being skipped, a newline character (i.e. \n) counts as a character.
+            return line.length();
     }
 
     // An overloaded method for the read method you will be writing!
@@ -73,11 +91,25 @@ public class FileStats {
         //    to its buffering mechanisms.
         //    a. HINT: BufferReader's Constructor takes another Reader as an argument. Consider FileReader
         //    b. REF: https://www.geeksforgeeks.org/java-io-bufferedreader-class-java/
+        FileReader fr = new FileReader(f);
+        BufferedReader bf = new BufferedReader(fr);
+
 
         // 2. Create a loop that uses your BufferedReader object to read the contents of your File object line-by-line
         //    and within the loop count the file's lines, words, and characters. Store them in the FileStats class's
         //    instance variables, so you can retrieve them in your main method.
         //    a. HINT: BufferedReader has a readLine method!!!
+        String ln;
+        while ((ln = bf.readLine()) != null) {
+
+            numLines += 1;
+            numWords += countWords(ln);
+            numChars += countChars(ln, skipWhiteSpace);
+        }
+
+        bf.close();
+        fr.close();
+
     }
 
     public int getNumLines() {
--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,55 @@
+package src;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // Create a scanner object
-
+            Scanner sc = new Scanner(System.in);
         // Write a loop that will ask the user to enter a file path to gather stats on,
-        // and continue until "Q" is entered.
+            String inp = "";
+            while (!inp.equalsIgnoreCase("Q")) {
+                System.out.println("Please enter the file path of your document, or press 'Q' to quit : ");
+                inp = sc.nextLine();
+                // and continue until "Q" is entered.
 
-            // Reference Java-Assignment-003 to see how to use the java.nio libraries to turn a String path into a File
+                // Reference Java-Assignment-003 to see how to use the java.nio libraries to turn a String path into a File
+                Path path = Paths.get(inp);
+                File myFile = path.toFile();
+                // Ask the user if they would like to skip whitespace
+                System.out.println("Great! Now, would you like to skip whitespace? (y/n) : ");
 
-            // Ask the user if they would like to skip whitespace
-
-            // Create a variable called skipWs that stores the user's response as a boolean
-
-            /*
-             * Within this try/catch block, which is used to handle possible errors thrown by the code in the try block,
-             * write code to get the line, word, and character count of the File object created above!
-             */
-            try {
-                // You will need to create a FileStats object by passing it the File object and your skipWs variable as args
-
-                // You will need to call the fs.read method, which you need to implement!
-
+                // Create a variable called skipWs that stores the user's response as a boolean
+                boolean skipsWs = !(sc.nextLine()).equalsIgnoreCase("N");
                 /*
-                 * You will access the FileStats object's getter methods to get the file's line, word, character count and
-                 * the files name. You should use a format specifier to print it all out similar to the following example:
-                 *
-                 * Stats: lines - 6, words - 46, chars - 237 /path/to/file/fileName.txt
+                 * Within this try/catch block, which is used to handle possible errors thrown by the code in the try block,
+                 * write code to get the line, word, and character count of the File object created above!
                  */
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
-            }
+                try {
+                    // You will need to create a FileStats object by passing it the File object and your skipWs variable as args
+                        FileStats fs = new FileStats(myFile, skipsWs);
 
+                    // You will need to call the fs.read method, which you need to implement!
+                        fs.read();
+                    /*
+                     * You will access the FileStats object's getter methods to get the file's line, word, character count and
+                     * the files name. You should use a format specifier to print it all out similar to the following example:
+                     *
+                     * Stats: lines - 6, words - 46, chars - 237 /path/to/file/fileName.txt
+                     */
+
+                    System.out.printf("Stats: lines = %d, words = %d, chars = %d --for file: %s\n", fs.getNumLines(), fs.getNumWords(), fs.getNumChars(), fs.getFileName());
+                    main(null);
+
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
     }
 }
